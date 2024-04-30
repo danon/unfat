c911070bd124ea2147e4dac4aa1402f379a4c9e0
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-04-30 06:40:20 +0200

    Add history of multiple meals, their weights and calories per 100g

--- src/unfat.ts
+++ src/unfat.ts
@@ -1,3 +1,7 @@
 export class History {
   currentCalories: number = 0;
+
+  addMeal(name: string, weight: number, caloriesPer100Grams: number): void {
+    this.currentCalories += Math.round(weight / 100 * caloriesPer100Grams);
+  }
 }
--- test/unit/unfat.test.ts
+++ test/unit/unfat.test.ts
@@ -3,9 +3,36 @@ import {strict as assert} from "node:assert";
 
 import {History} from "../../src/unfat.js";
 
-suite('unit/', (): void => {
-  test('start', (): void => {
-    const history = new History();
-    assert.equal(history.currentCalories, 0);
+suite('unit/', () => {
+  suite('history', () => {
+    test('start', (): void => {
+      const history = new History();
+      assert.equal(history.currentCalories, 0);
+    });
+
+    test('first meal', (): void => {
+      const history = new History();
+      history.addMeal('apple', 150, 50);
+      assert.equal(history.currentCalories, 75);
+    });
+
+    test('calories as integer', (): void => {
+      const history = new History();
+      history.addMeal('apple', 154, 10);
+      assert.equal(history.currentCalories, 15);
+    });
+
+    test('calories as integer, round up', (): void => {
+      const history = new History();
+      history.addMeal('apple', 156, 10);
+      assert.equal(history.currentCalories, 16);
+    });
+
+    test('second meal', (): void => {
+      const history = new History();
+      history.addMeal('apple', 70, 100);
+      history.addMeal('banana', 120, 100);
+      assert.equal(history.currentCalories, 190);
+    });
   });
 });
