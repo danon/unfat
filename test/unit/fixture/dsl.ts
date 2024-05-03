66de2aeb6dcb440d9f610a0af621c0b28f982d46
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-05-03 05:45:25 +0200

    Refactor view tests with DSL

---
+++ test/unit/fixture/dsl.ts
@@ -0,0 +1,60 @@
+import type {AsyncFunc, Context} from "mocha";
+import {strict} from "node:assert";
+
+import {Browser} from "./browser.js";
+
+type Command<T> = (browser: Browser) => Promise<T>;
+
+export function browserView(commands: Command<void>[]): AsyncFunc {
+  return async function (this: Context): Promise<void> {
+    this.timeout(10000);
+    await executeInBrowser(each(commands));
+  };
+}
+
+async function executeInBrowser(command: Command<void>): Promise<void> {
+  const browser = new Browser();
+  await browser.open();
+  try {
+    await command(browser);
+  } finally {
+    await browser.close();
+  }
+}
+
+function each(commands: Command<void>[]): Command<void> {
+  return async function (browser: Browser): Promise<void> {
+    for (const command of commands) {
+      await command(browser);
+    }
+  };
+}
+
+function execute<T>(javaScript: string, args: string[]): Command<T> {
+  return browser => browser.execute(javaScript, args) as Promise<T>;
+}
+
+export function currentCalories(): Command<number> {
+  return execute("return parseInt(document.querySelector('#currentCalories').textContent);", []);
+}
+
+export function typeCalories(calories: string): Command<void> {
+  return execute("document.querySelector('input#calories').value = arguments[0];", [calories]);
+}
+
+export function submitMeal(): Command<void> {
+  return execute("document.querySelector('button').click();", []);
+}
+
+export function addMealWithCalories(calories: number): Command<void> {
+  return each([
+    typeCalories(calories.toString()),
+    submitMeal(),
+  ]);
+}
+
+export function assert<T>(command: Command<T>, expected: T): Command<void> {
+  return async function (browser: Browser): Promise<void> {
+    strict.deepEqual(await command(browser), expected);
+  };
+}
--- test/unit/view.test.ts
+++ test/unit/view.test.ts
@@ -1,63 +1,25 @@
 import {suite, test} from "mocha";
-import {strict as assert} from "node:assert";
 
-import {Browser} from "./fixture/browser.js";
+import {addMealWithCalories, assert, browserView, currentCalories} from "./fixture/dsl.js";
 
 suite('unit/', () => {
   suite('view', () => {
-    test('zero calories', async function () {
-      this.timeout(10000);
-      const browser = new Browser();
-      await browser.open();
-      const currentCalories = await getCurrentCalories(browser);
-      await browser.close();
-      assert.equal(currentCalories, 0);
-    });
+    test('zero calories',
+      browserView([
+        assert(currentCalories(), 0),
+      ]));
 
-    test('show meal calories', async function () {
-      this.timeout(10000);
-      // given
-      const browser = new Browser();
-      await browser.open();
-      // when
-      await addMealWithCalories(browser, 75);
-      // then
-      const currentCalories = await getCurrentCalories(browser);
-      await browser.close();
-      assert.equal(currentCalories, 75);
-    });
+    test('show meal calories',
+      browserView([
+        addMealWithCalories(75),
+        assert(currentCalories(), 75),
+      ]));
 
-    test('sum meal calories', async function () {
-      this.timeout(10000);
-      // given
-      const browser = new Browser();
-      await browser.open();
-      await addMealWithCalories(browser, 75);
-      // when
-      await addMealWithCalories(browser, 35);
-      // then
-      const currentCalories = await getCurrentCalories(browser);
-      await browser.close();
-      assert.equal(currentCalories, 110);
-    });
+    test('sum meal calories',
+      browserView([
+        addMealWithCalories(75),
+        addMealWithCalories(35),
+        assert(currentCalories(), 110),
+      ]));
   });
 });
-
-function getCurrentCalories(browser: Browser): Promise<unknown> {
-  return browser.execute("return parseInt(document.querySelector('#currentCalories').textContent);", []);
-}
-
-async function addMealWithCalories(browser: Browser, calories: number): Promise<void> {
-  await typeCalories(browser, calories.toString());
-  await submitMeal(browser);
-}
-
-async function typeCalories(browser: Browser, caloriesText: string): Promise<void> {
-  await browser.execute(
-    "document.querySelector('input').value = arguments[0];",
-    [caloriesText]);
-}
-
-async function submitMeal(browser: Browser): Promise<void> {
-  await browser.execute("document.querySelector('button').click();", []);
-}
