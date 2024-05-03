d6f7471f83f71f7a9006b5c3f9181b685b17855a
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-05-03 05:35:30 +0200

    Display meal calories

--- src/view.ts
+++ src/view.ts
@@ -6,3 +6,15 @@ const preview = document.createElement('span');
 preview.textContent = '0';
 preview.id = 'currentCalories';
 label.appendChild(preview);
+
+const caloriesInput = document.createElement('input');
+caloriesInput.id = 'calories';
+caloriesInput.placeholder = 'calories';
+document.body.appendChild(caloriesInput);
+
+const button = document.createElement('button');
+button.textContent = 'Add meal';
+button.addEventListener('click', () => {
+  preview.textContent = caloriesInput.value;
+});
+document.body.appendChild(button);
--- test/unit/view.test.ts
+++ test/unit/view.test.ts
@@ -9,9 +9,41 @@ suite('unit/', () => {
       this.timeout(10000);
       const browser = new Browser();
       await browser.open();
-      const currentCalories = await browser.execute("return parseInt(document.querySelector('#currentCalories').textContent);", []);
+      const currentCalories = await getCurrentCalories(browser);
       await browser.close();
       assert.equal(currentCalories, 0);
     });
+
+    test('show meal calories', async function () {
+      this.timeout(10000);
+      // given
+      const browser = new Browser();
+      await browser.open();
+      // when
+      await addMealWithCalories(browser, 75);
+      // then
+      const currentCalories = await getCurrentCalories(browser);
+      await browser.close();
+      assert.equal(currentCalories, 75);
+    });
   });
 });
+
+function getCurrentCalories(browser: Browser): Promise<unknown> {
+  return browser.execute("return parseInt(document.querySelector('#currentCalories').textContent);", []);
+}
+
+async function addMealWithCalories(browser: Browser, calories: number): Promise<void> {
+  await typeCalories(browser, calories.toString());
+  await submitMeal(browser);
+}
+
+async function typeCalories(browser: Browser, caloriesText: string): Promise<void> {
+  await browser.execute(
+    "document.querySelector('input').value = arguments[0];",
+    [caloriesText]);
+}
+
+async function submitMeal(browser: Browser): Promise<void> {
+  await browser.execute("document.querySelector('button').click();", []);
+}
